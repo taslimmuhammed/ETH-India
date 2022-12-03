34e53060a47b7b23792b356353d74a15656faee2
taslimmuhammed <taslimmuhammed67@gmail.com>
2022-12-03 17:46:23 +0530

    routes and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Web3Storage, getFilesFromPath } from 'web3.storage'
+import React, {useState, useContext} from "react"
+import { EthersContext } from './Context/EthersContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home';
+import Create from './Pages/Create';
+import ViewPage from './Pages/ViewPage';
+import Transfer from './Components/Transfer';
+import MyWorks from './Components/MyWorks';
 
 function App() {
+  const {Blocks, setBlocks}= useContext(EthersContext)
+  const [Files, setFiles] = useState()
+  let filesArray =[]
+  let files= []
+  async function main(){
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDhDRmRhYjQ2NUE4QzAwRWE0ZWE5YTMzY2Y1N0NkQzdhRmUzMTllMzUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTAwNDc1MzU5NjksIm5hbWUiOiJ0ZXN0In0.H0u5Ktl7sXELTGAYxAkQRzw5uh_JHsxzJtN5mbepLhE"
+    const storage = new Web3Storage({ token })
+    let cid = await storage.put(Files)
+    console.log(cid)
+    setBlocks("Hi")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen">
+  
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<Home/>}></Route>
+          <Route path='/create'  element={<Create/>}></Route>
+          <Route path='/view'  element={<ViewPage/>}></Route>
+          <Route path='/transfer'  element={<Transfer/>}></Route>
+          <Route path='/myworks'  element={<MyWorks/>}></Route>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
